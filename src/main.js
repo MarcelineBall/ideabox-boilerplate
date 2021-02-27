230e0e1fd72b0b75b1056319c08e8c0aeb1ee2db
Marceline Ball <marchettiball@gmail.com>
2021-02-26 18:07:55 -0700

    Add deletecard function, and refactor

--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ var saveButton = document.getElementById('saveButton')
 
 createIdeaContainer.addEventListener('click', saveIdea);
 window.addEventListener('load', checkTextInputs);
+// window.addEventListener('click', deleteCard);
 createIdeaContainer.addEventListener('keyup', checkTextInputs);
+savedIdeaContainer.addEventListener('click', deleteCard);
 
 function saveIdea() {
   var titleText = titleInput.value;
@@ -17,30 +19,7 @@ function saveIdea() {
   if (event.target.id === 'saveButton') {
     var idea = new Idea(titleText, bodyText);
     idea.saveToStorage();
-    savedIdeaContainer.innerHTML = '';
-    for (var i = 0; i < ideas.length; i++) {
-      savedIdeaContainer.innerHTML +=
-        `<output id="${ideas[i].id}" class="idea">
-    <header class="idea-header">
-      <button class="favorite-button" id="favoriteButton">
-        <img src="assets/icons/star-active.svg" alt="favorite-star">
-      </button>
-      <button class="close-button">
-        <img src="assets/icons/menu-close.svg" alt="menu-close">
-      </button>
-    </header>
-    <div class="idea-body">
-      <h4>${ideas[i].title}</h4>
-      <p>${ideas[i].body}</p>
-    </div>
-    <div class="comment-button-wrapper">
-      <button id="commentButton">
-        <img src="assets/icons/comment.svg" alt="">
-      </button>
-      <p>Comment</p>
-    </div>
-  </output>`;
-    }
+    renderIdeaCards();
     document.getElementById('titleInput').value = '';
     document.getElementById('bodyInput').value = '';
   };
@@ -55,3 +34,48 @@ function checkTextInputs() {
     saveButton.disabled = false;
   };
 };
+
+function renderIdeaCards() {
+  savedIdeaContainer.innerHTML = '';
+  for (var i = 0; i < ideas.length; i++) {
+    savedIdeaContainer.innerHTML +=
+      `<output id="${ideas[i].id}" class="idea">
+  <header class="idea-header">
+    <button class="favorite-button" id="favoriteButton">
+      <img src="assets/icons/star.svg" alt="favorite-star">
+    </button>
+    <button id="closeButton" class="close-button">
+      <img src="assets/icons/menu-close.svg" alt="menu-close">
+    </button>
+  </header>
+  <div class="idea-body">
+    <h4>${ideas[i].title}</h4>
+    <p>${ideas[i].body}</p>
+  </div>
+  <div class="comment-button-wrapper">
+    <button id="commentButton">
+      <img src="assets/icons/comment.svg" alt="">
+    </button>
+    <p>Comment</p>
+  </div>
+ </output>`;
+  };
+};
+// 1. target the delete button with event.target === delete button
+// 2. access the <output> element's id and match it to the ideas array
+// 3. run some type of function/method to remove that from the ideas array
+// 4. re-render the cards
+
+function deleteCard() {
+  if (event.target.id === 'closeButton') {
+    console.log(event.currentTarget.querySelector('output').id);
+    var cardId = event.currentTarget.querySelector('output').id;
+    for (var i = 0; i < ideas.length; i++) {
+      if (ideas[i].id.toString() === cardId) {
+        console.log('true');
+        ideas.splice(i, 1);
+      }
+      renderIdeaCards();
+    }
+  }
+}
