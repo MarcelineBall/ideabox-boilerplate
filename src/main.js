5654db52d99660d7aee1f3f6ae0900c90497ccd3
Eva <>
2021-03-01 12:00:20 -0700

    Refactor filter favorites

--- src/main.js
+++ src/main.js
@@ -146,45 +146,36 @@ function filterFavoriteIdeas() {
   if (event.target.id === 'showStarredIdeasButton') {
     if (event.target.innerText === 'Show Starred Ideas') {
       event.target.innerText = 'Show All Ideas';
-      var redStarShowOrNo;
-      savedIdeaContainer.innerHTML = '';
-      for (var i = 0; i < favoriteIdeas.length; i++) {
-        if (favoriteIdeas[i].isFavorite) {
-          redStarShowOrNo = ''
-        } else {
-          redStarShowOrNo = 'hidden'
-        };
-        savedIdeaContainer.innerHTML +=
-          `<output id="${favoriteIdeas[i].id}" class="idea">
-    <header class="idea-header">
-      <button class="favorite-button" id="favoriteButton">
-        <div class="star-container">
-        <img class="empty-star" id="favoriteStar" src="assets/icons/star.svg" alt="favorite-star">
-        <img class="active-star ${redStarShowOrNo}" id="favoriteStarActive" src="assets/icons/star-active.svg" alt="favorite-star-active">
-        </div>
-      </button>
-      <button id="closeButton" class="close-button">
-        <img id="menuClose" src="assets/icons/menu-close.svg" alt="menu-close">
-      </button>
-    </header>
-    <div class="idea-body">
-      <h4>${favoriteIdeas[i].title}</h4>
-      <p>${favoriteIdeas[i].body}</p>
-    </div>
-    <div class="comment-button-wrapper">
-      <button id="commentButton">
-        <img src="assets/icons/comment.svg" alt="comment-button">
-      </button>
-      <p>Comment</p>
-    </div>
-   </output>`;
-      };
+      hideNonFavorites();
     } else if (event.target.innerText === 'Show All Ideas') {
       event.target.innerText = 'Show Starred Ideas';
-      renderIdeaCards();
+      showNonFavorites();
     };
   };
 };
+
+function hideNonFavorites() {
+  var cardsNodeList = document.querySelectorAll('output');
+  var cardsArray = Array.from(cardsNodeList);
+  for (var i = 0; i < cardsArray.length; i++) {
+    var redStar = cardsArray[i].children[0].children[0].children[0].children[1];
+    if (redStar.classList.contains('hidden')) {
+      cardsArray[i].style.display = 'none'
+    };
+  };
+};
+
+function showNonFavorites() {
+  var cardsNodeList = document.querySelectorAll('output');
+  var cardsArray = Array.from(cardsNodeList);
+  for (var i = 0; i < cardsArray.length; i++) {
+    var redStar = cardsArray[i].children[0].children[0].children[0].children[1];
+    if (redStar.classList.contains('hidden')) {
+      cardsArray[i].style.display = 'inherit'
+    };
+  };
+};
+
 
 function filterSearch() {
   var searchInput = document.getElementById('searchInput');
